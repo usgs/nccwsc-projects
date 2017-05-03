a5ec908ba34ec1b9647bb50072406e929e67bbf7
Thomas W Miller <twmiller@usgs.gov>
2017-05-03 10:31:25 -0600

    Fixed map to use layers and BB coming out of the SB WMS.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import { LeafletMapModel } from './map.model';
 import * as L from 'leaflet';
+import * as X2JS from "x2js";
 
 @Component({
   selector: 'sb-map',
@@ -10,21 +11,29 @@ import * as L from 'leaflet';
 })
 
 export class MapComponent {
-  @Input() mapurl: string;
-  LAYER_OCM: any;
-  LAYER_OSM: any;
-  model: any;
-  shapename: any;
-  shapename2: any;
-  shapename3: any;
-  projectshape_wms_url: any;
-  sb_footprint: any;
-  sb_boundingbox: any;
-  sb_children: any;
-  arrMapCenterLatLong: any;
-  xms_capabilities;
-  parseString = require('xml2js').parseString;
-  
+  @Input() mapUrl: string
+  LAYER_OCM: any
+  LAYER_OSM: any
+  model: any
+  shapename: any
+  shapename2: any
+  shapename3: any  
+  projectshape_wms_url: any
+  sb_footprint: any
+  sb_boundingbox: any
+  sb_children: any
+  arrMapCenterLatLong: any
+  xmsCapabilities: any
+  fitBounds: any
+  center: any
+  wmsLayers: any = []
+  layers: L.Layer[]
+  layersControl: any
+  options: any
+  render: boolean = false;
+  layersFromWMS:any = {}
+  layersArray:any = []
+
   defineBaseLayers(){
     this.LAYER_OCM = {
       id: 'opencyclemap',
@@ -34,7 +43,7 @@ export class MapComponent {
         maxZoom: 18,
         attribution: 'Open Cycle Map'
       })
-    };
+    }
     this.LAYER_OSM = {
       id: 'openstreetmap',
       name: 'Open Street Map',
@@ -43,121 +52,152 @@ export class MapComponent {
         maxZoom: 18,
         attribution: 'Open Street Map'
       })
-    };
+    }
   }
 
   defineOverlays() {
-  this.shapename = 'sb:footprint';
-  this.projectshape_wms_url = this.mapurl.replace(/service=wms&request=getcapabilities&version=1.3.0/, '');
-        console.log(this.mapurl);
-  //this.getWMSCapabilities(this.mapurl).then(data=>{
-  
-  this.sb_footprint = {
-    id: 'sb_footprint',
-    name: 'Footprint',
-    enabled: true,
-    layer: L.tileLayer.wms(this.projectshape_wms_url, {
-          layers: this.shapename,
-          format: 'image/png',
-          transparent: true,
-          enabled: true})
+    this.projectshape_wms_url = this.mapUrl.replace(/service=wms&request=getcapabilities&version=1.3.0/, '');
+    if (this.xmsCapabilities['WMS_Capabilities'].Capability.Layer.Layer && (this.xmsCapabilities['WMS_Capabilities'].Capability.Layer.Layer.length > 0)) {
+      for (var layer of this.xmsCapabilities['WMS_Capabilities'].Capability.Layer.Layer) {
+        this.layersArray.push({
+          id: layer.Name, 
+          name: layer.Name, 
+          enabled: true, 
+          layer: L.tileLayer.wms(this.projectshape_wms_url, {
+            layers: layer.Name, 
+            format: 'image/png', 
+            transparent: true, 
+            enabled: true
+          })
+        })
+        this.layersFromWMS[layer.Name] = {
+          id: layer.Name, 
+          name: layer.Name, 
+          enabled: true, 
+          layer: L.tileLayer.wms(this.projectshape_wms_url, {
+            layers: layer.Name, 
+            format: 'image/png', 
+            transparent: true, 
+            enabled: true
+          })
+        }
+      }
     }
-
-  this.sb_boundingbox = {
-    id: 'sb_boundingbox',
-    name: 'Bounding Box',
-    enabled: true,
-    layer: L.tileLayer.wms(this.projectshape_wms_url, {
-          layers: this.shapename2,
-          format: 'image/png',
-          transparent: true,
-          enabled: true})
-    }
-
-  this.sb_children = {
-    id: 'sb_children',
-    name: 'Children',
-    enabled: true,
-    layer: L.tileLayer.wms(this.projectshape_wms_url, {
-          layers: this.shapename3,
-          format: 'image/png',
-          transparent: true,
-          enabled: true})
-    }
-
- //   })
+    this.defineBaseLayers()
+    this.defineModel()
+    this.onApply()
   }
-  
+
+  parseWMSGeographicBoundingBox(boundingBox) {
+    return [
+      [boundingBox.southBoundLatitude, boundingBox.westBoundLongitude], // SW
+      [boundingBox.northBoundLatitude, boundingBox.eastBoundLongitude] // NE
+    ];
+  }
 
   getWMSCapabilities(mapUrl) {
+    var x2js = new X2JS()
     return this.http.get(mapUrl).toPromise()
       .then(response => {
-        this.parseString(response.text(), function (err, result) {
-          var object = result;
-          if (typeof(object) != 'undefined') {
-            this.xms_capabilties = object;
+        var xmlData = response.text()
+        this.xmsCapabilities = x2js.xml2js(xmlData);
+        if (this.xmsCapabilities) {
+          if (this.xmsCapabilities && this.xmsCapabilities['WMS_Capabilities'] && this.xmsCapabilities['WMS_Capabilities'].Capability && this.xmsCapabilities['WMS_Capabilities'].Capability.Layer) {
+            var thisLayer = this.xmsCapabilities['WMS_Capabilities'].Capability.Layer;
+            if (thisLayer.Layer) {
+              for (var i = 0, l = thisLayer.Layer.length; i < l; i++) {
+                  var wms_layer = thisLayer.Layer[i];
+                  if (wms_layer && wms_layer.EX_GeographicBoundingBox) {
+                      this.wmsLayers[wms_layer.Name] = this.parseWMSGeographicBoundingBox(wms_layer.EX_GeographicBoundingBox);
+                  } else {
+                      this.wmsLayers[wms_layer.Name] = null;
+                  }
+              }
           }
-        });
-      })
-      .catch(this.handleError);
+          if (thisLayer.EX_GeographicBoundingBox) {
+            this.fitBounds = this.parseWMSGeographicBoundingBox(thisLayer.EX_GeographicBoundingBox);
+          }            
+        }
+      } else {
+
+        console.log("We have no json data. :/");
+
+      }
+
+      console.log("Why should this be last?")
+      this.defineOverlays()
+      this.render = true
+
+    })
+    .catch(this.handleError);
   }
 
   defineModel() {
-    // Form model object
+    var overlayArray = []   
+    for (var key in this.layersFromWMS) {
+      if (this.layersFromWMS.hasOwnProperty(key)) {
+        overlayArray.push(this.layersFromWMS[key]);
+      }
+    }
     this.model = new LeafletMapModel(
       [ this.LAYER_OSM, this.LAYER_OCM ],
       this.LAYER_OCM.id,
-      [ this.sb_footprint, this.sb_boundingbox, this.sb_children ]
+      overlayArray
     );
   }
   
-  // Values to bind to Leaflet Directive
-  layers: L.Layer[];
-  //tileLayers:L.tileLayer[];
-  layersControl: any;
-  options = {
-    zoom: 3,
-    //center: L.latLng([ 56.37692652561914, -137.8891296838142 ])
-    center: L.latLng([ 39.8282, -98.5795 ])
-  };
+
+  mapInit() {
+    this.options = {
+      zoom: 3,
+      center: L.latLng([ 39.8282, -98.5795 ]),
+      fitBounds: this.fitBounds
+    }
+  }
 
   constructor(private http: Http) {
   }
 
   ngOnInit() {
-    this.defineBaseLayers();
-    this.defineOverlays();
-    this.defineModel();
-    this.onApply();
+    this.getWMSCapabilities(this.mapUrl)
   }
   
   private handleError(error: any): Promise < any > {
-    console.error('An error occurred', error);
     return Promise.reject(error.message || error);
   }
 
   onApply() {
-    // Get the active base layer
-    let baseLayer = this.model.baseLayers.find((l) => { return l.id === this.model.baseLayer; });
-    // Get all the active overlay layers
+    this.mapInit()
+    let baseLayer = this.model.baseLayers.find((l) => { 
+      return l.id === this.model.baseLayer 
+    })
     let newLayers = this.model.overlayLayers
-      .filter((l) => { return l.enabled; })
-      .map((l) => { return l.layer; });
-    newLayers.unshift(baseLayer.layer);
-    this.layers = newLayers;
+      .filter((l) => { 
+        return l.enabled 
+      })
+      .map((l) => { 
+        return l.layer 
+      })
+    newLayers.unshift(baseLayer.layer)
+    this.layers = newLayers
+    var overlays = {}
+    for (var thisLayer of this.layersFromWMS) {
+      overlays[thisLayer.name] = thisLayer.layer
+    }
+
+    for (var key in this.layersFromWMS) {
+      if (this.layersFromWMS.hasOwnProperty(key)) {
+        overlays[key] = this.layersFromWMS[key].layer
+      }
+    }
     this.layersControl = {
       baseLayers: {
         'Open Street Map': this.LAYER_OSM.layer,
         'Open Cycle Map': this.LAYER_OCM.layer
       },
-      overlays: {
-        'SB Footprint': this.sb_footprint.layer,
-        'SB Bounding Box': this.sb_boundingbox.layer,
-        'SB Children': this.sb_children.layer
-      }
+      overlays: overlays
 
-    };
-
+    }
     return false;
   }
 
