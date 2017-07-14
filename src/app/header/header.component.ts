2b1b36482a9db73043fbe5d7e2f9675acad899bc
Shapiro <eshapiro@usgs.gov>
2017-07-14 13:06:46 -0600

    in header component added environmental dependancy for the url and menu links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NccwscDrupalService } from '../nccwsc-drupal.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-header',
@@ -14,22 +16,48 @@ export class HeaderComponent implements OnInit {
   isCollapsed = true
   constructor(private nccwscDrupal: NccwscDrupalService) { }
 
+setTitleUrl(){
+  // sets header urls
+let titleURL = "";
+   if (environment.production==false){
+          titleURL = "https://my-beta.usgs.gov/nccwsc";
+       }
+   else{
+      titleURL = "https://nccwsc.usgs.gov/";
+   }
+
+return titleURL;
+}
+
+
+
+
+
   fixLink(link) {
-console.log(link);
+     // sets url prefix deppending if it prod or beta
+let url_prefix = "";
+      if (environment.production==false){
+             url_prefix = "/nccwsc/";
+          }
+      else{
+         url_prefix = "/";
+      }
+
     if ((link.substr(0, 24) == 'https://nccwsc.usgs.gov/')  || (link.substr(0, 32) == 'https://my-beta.usgs.gov/nccwsc/')) {
       if ((link.substr(0,43) == 'https://my-beta.usgs.gov/nccwsc/projects/#/') || (link.substr(0, 35) == 'https://nccwsc.usgs.gov/projects/#/')) {
-//this for angular app links
+// angular app links
         return link
       } else {
         return link.replace('https://nccwsc.usgs.gov/', '/');
       }
     } else {
-  //this for node/# links
-        return "/nccwsc/"+link;
+  //node/# links
+        return url_prefix + link;
       }
   }
 
   ngOnInit() {
+
      this.nccwscDrupal.loadMenu().subscribe(data => {
        data[0].url = "//";
        this.nccwscMenu = data;
