6279681f0acaa0395b20eb311780d8a064eb56e9
Thomas Miller <twmiller@usgs.gov>
2017-08-31 13:35:45 -0600

    Bumped version, styled search info

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  version: '1.0.21a-beta',
+  version: '1.0.22a-beta',
   production: true,
   baseURL: 'https://my-beta.usgs.gov/nccwsc',
+  serviceURL: 'https://my-beta.usgs.gov/nccwsc-service/',
   projectsPath: '/projects/#',
   urlPrefix: "/nccwsc/"
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  version: '1.0.21a',
+  version: '1.0.22a',
   production: true,
   baseURL: 'https://nccwsc.usgs.gov',
   serviceURL: 'https://nccwsc.usgs.gov/nccwsc-service',
