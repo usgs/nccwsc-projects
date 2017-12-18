9aa4d88595a5e7a063986071842ffb5976cb993a
GS\jlabadie <jlabadie@usgs.gov>
2017-12-18 13:33:59 -0700

    Make the version of Angular code the same as Drupal.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  version: '1.1.0',
+  version: '4.1.0',
   production: true,
   baseURL: 'https://nccwsc.usgs.gov',
   serviceURL: 'https://nccwsc.usgs.gov/nccwsc-service',
