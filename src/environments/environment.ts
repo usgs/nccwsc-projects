3f532d4ee32d106f8731a89765c651934037cf42
GS\jlabadie <jlabadie@contractor.usgs.gov>
2018-03-02 10:40:00 -0700

    correction to baseurl path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   version: '4.1.2-dev',
   production: false,
-  baseURL: 'https://ede.cr.usgs.gov/nccwsc/',
+  baseURL: 'https://ede.cr.usgs.gov/nccwsc',
   serviceURL: 'https://ede.cr.usgs.gov/nccwsc-service',
   projectsPath: '/projects/#',
   urlPrefix: "/nccwsc/"
