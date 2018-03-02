3d10dc8336398e4d0d0eac3bbb8cdea54e6bd918
GS\jlabadie <jlabadie@contractor.usgs.gov>
2018-03-02 10:30:09 -0700

    Include ede in environment

--- src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  version: '4.1.2-dev',
-  production: true,
-  baseURL: 'https://ede.cr.usgs.gov/nccwsc/',
-  serviceURL: 'https://ede.cr.usgs.gov/nccwsc-service',
-  projectsPath: '/projects/#',
-  urlPrefix: "/nccwsc/"
-};
