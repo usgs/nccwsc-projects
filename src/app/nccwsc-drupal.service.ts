44564ad523a99d3ac47b5eab1800dbcd308df82d
jlabadie <jlabadie@contractor.usgs.gov>
2018-07-10 16:27:41 -0600

    Code cleanup

--- src/app/nccwsc-drupal.service.ts
+++ src/app/nccwsc-drupal.service.ts
@@ -2,9 +2,6 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import { environment } from '../environments/environment';
 
-//import {Observable} from 'rxjs/Observable';
-//import 'rxjs/Rx';
-
 @Injectable()
 export class NccwscDrupalService {
 
@@ -12,8 +9,8 @@ export class NccwscDrupalService {
 
   loadMenu() {
     // Load menu from ncccwsc drupal site here...
-    var nccwscMenuUrl = environment.baseURL + '/menu-export';
-    return this.http.get(nccwscMenuUrl).map((res:Response) => res.json());
+    const nccwscMenuUrl = environment.baseURL + '/menu-export';
+    return this.http.get(nccwscMenuUrl).map((res: Response) => res.json());
   }
 
 }
