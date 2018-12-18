984af7d4007f5f2fdd37425e9e53000d3239e84a
Urpsis <aurpsis@usgs.gov>
2018-12-18 11:02:20 -0700

    cscs buttons

--- src/app/cscs/cscs.component.ts
+++ src/app/cscs/cscs.component.ts
@@ -8,24 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class CscsComponent implements OnInit {
 
   csc_ids = [
+    {'id': 'alaska', 'name' : 'Alaska'},
+    {'id': 'northwest', 'name' : 'Northwest'},
+    {'id': 'north-central', 'name' : 'North Central'},
+    {'id': 'northeast', 'name' : 'Northeast'},
+    {'id': 'pacific-islands', 'name' : 'Pacific Islands'},
+    {'id': 'southwest', 'name' : 'Southwest'},
+    {'id': 'south-central', 'name' : 'South Central'},
+    {'id': 'southeast', 'name' : 'Southeast'},
     {'id': 'national-casc', 'name' : 'National CASC'},
-    {'id': 'alaska', 'name' : 'Alaska CASC'},
-    {'id': 'north-central', 'name' : 'North Central CASC'},
-    {'id': 'northeast', 'name' : 'Northeast CASC'},
-    {'id': 'northwest', 'name' : 'Northwest CASC'},
-    {'id': 'pacific-islands', 'name' : 'Pacific Islands CASC'},
-    {'id': 'south-central', 'name' : 'South Central CASC'},
-    {'id': 'southeast', 'name' : 'Southeast CASC'},
-    {'id': 'southwest', 'name' : 'Southwest CASC'}
   ]
 
   topic_ids = [
-    {'id': 'drought-fire-extremes', 'name': 'Drought, Fire and Extreme Weather'},
-    {'id': 'science-tools', 'name': 'Science Tools for Managers'},
     {'id': 'landscapes', 'name': 'Landscapes'},
-    {'id': 'native-communities', 'name': 'Native Communities'},
+    {'id': 'drought-fire-extremes', 'name': 'Drought, Fire and Extreme Weather'},
+    {'id': 'wildlife-plants', 'name': 'Wildlife and Plants'},
     {'id': 'water-coasts-ice', 'name': 'Water, Coasts and Ice'},
-    {'id': 'wildlife-plants', 'name': 'Wildlife and Plants'}
+    {'id': 'native-communities', 'name': 'Native Communities'},
+    {'id': 'science-tools', 'name': 'Science Tools for Managers'},
   ]
 
   constructor() { }
