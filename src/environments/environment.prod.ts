792a775c98275bafdb5f2195ddf2d3ae7af3f2d5
John L Long <jllong@usgs.gov>
2019-04-09 12:24:37 -0600

    Update back end service url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   version: '4.3.1',
   production: true,
   baseURL: 'https://casc.usgs.gov',
-  serviceURL: 'https://casc.usgs.gov/casc-service',
+  serviceURL: 'https://my.usgs.gov/casc-service',
   sbmainURL: 'https://www.sciencebase.gov',
   projectsPath: '/projects/#',
   urlPrefix: '/'
