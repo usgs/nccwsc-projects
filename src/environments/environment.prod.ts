d2a4bca2deb255bc5dec66ddb9c83ece62dd706f
Bruce Crevensten <bruce.crevensten@gmail.com>
2019-04-10 16:25:24 -0700

    update backend and root URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   version: '4.3.1',
   production: true,
-  baseURL: 'https://casc.usgs.gov',
-  serviceURL: 'https://casc.usgs.gov/casc-service',
+  baseURL: 'http://cascprojects.org',
+  serviceURL: 'https://my.usgs.gov/casc-service',
   sbmainURL: 'https://www.sciencebase.gov',
   projectsPath: '/projects/#',
   urlPrefix: '/'
