96eb0143755797c8b6812ca7ca890c1a7586b3fd
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-11-30 15:26:32 -0800

    Layout tweaks 2 (#38)
    * Multiple fixes per feedback
    
    * Finishes changes to be made based on feedback from today's discussion
    
    * Interim commit with further updates to styling
    
    Co-authored-by: BobTorgerson <rltorgerson@alaska.edu>

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -7,10 +7,11 @@ import { TitleLinkComponent } from '../title-link/title-link.component';
 import { Location } from '@angular/common';
 import { UrlService } from '../url.service';
 
+
 @Component({
-  selector: 'app-topics',
-  templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.scss']
+  selector: "app-topics",
+  templateUrl: "./topics.component.html",
+  styleUrls: ["./topics.component.scss"]
 })
 export class TopicsComponent implements OnInit {
   sub = null;
@@ -18,83 +19,98 @@ export class TopicsComponent implements OnInit {
   page_title = null;
 
   topics = {
-    'drought-fire-extremes': '588244b0e4b0b3d9add24391',
-    'science-tools': '5b6212e7e4b03f4cf7599b82',
-    'landscapes': '5882456be4b0b3d9add24395',
-    'indigenous-peoples': '588246dae4b0b3d9add243a1',
-    'water-coasts-ice': '5882464ce4b0b3d9add2439a',
-    'wildlife-plants': '58824220e4b0b3d9add2438b'
-  }
+    "drought-fire-extremes": "588244b0e4b0b3d9add24391",
+    "science-tools": "5b6212e7e4b03f4cf7599b82",
+    landscapes: "5882456be4b0b3d9add24395",
+    "indigenous-peoples": "588246dae4b0b3d9add243a1",
+    "water-coasts-ice": "5882464ce4b0b3d9add2439a",
+    "wildlife-plants": "58824220e4b0b3d9add2438b"
+  };
   topic_names = {
-    'drought-fire-extremes': 'Drought, Fire and Extreme Weather',
-    'science-tools': 'Science Tools for Managers',
-    'landscapes': 'Landscapes',
-    'indigenous-peoples': 'Indigenous Peoples',
-    'water-coasts-ice': 'Water, Coasts and Ice',
-    'wildlife-plants': 'Wildlife and Plants'
-  }
+    "drought-fire-extremes": "Drought, Fire and Extreme Weather",
+    "science-tools": "Science Tools for Managers",
+    landscapes: "Landscapes",
+    "indigenous-peoples": "Indigenous Peoples",
+    "water-coasts-ice": "Water, Coasts and Ice",
+    "wildlife-plants": "Wildlife and Plants"
+  };
 
   settings = {
     columns: {
       fiscal_year: {
-        title: 'Year',
-        width: '6%',
+        title: "FY",
+        width: "6%"
       },
       title: {
-        title: 'Title',
-        type: 'custom',
-        renderComponent: TitleLinkComponent,
+        title: "Title",
+        type: "custom",
+        renderComponent: TitleLinkComponent
       },
-        csc_name: {
-        title: 'CASC',
+      csc_name: {
+        title: "CASC"
       },
       subtopics_formatted: {
-        title: 'Subtopic(s)',
-        type: 'html',
+        title: "Subtopic(s)",
+        type: "html"
       },
       status: {
-        title: 'Status',
-        width: '10%',
+        title: "Status",
+        width: "10%"
       }
     },
     actions: false,
-    hideSubHeader:true,
-    pager:{
-      display:false
+    hideSubHeader: true,
+    pager: {
+      display: false
     }
     // this.source.setSort([{ field: 'id', direction: 'asc' }]);
   };
 
-
   topics_url = environment.baseURL;
-  project_url = environment.baseURL + '/project';
+  project_url = environment.baseURL + "/project";
 
   url: any;
-  subtopics = ['All Subtopics'];
-  fiscal_years = ['All Fiscal Years'];
-  statuses = ['All Statuses'];
-  cscs=['All CASCs'];
-  types=['Project'];
-  current_subtopic = 'All Subtopics';
-  current_type = 'Project';
-  current_fy = 'All Fiscal Years';
-  current_status = 'All Statuses';
-  current_csc = 'All CASCs';
+  subtopics = ["All Subtopics"];
+  fiscal_years = ["All Fiscal Years"];
+  statuses = ["All Statuses"];
+  cscs = ["All CASCs"];
+  types = ["Project"];
+  current_subtopic = "All Subtopics";
+  current_type = "Project";
+  current_fy = "All Fiscal Years";
+  current_status = "All Statuses";
+  current_csc = "All CASCs";
   topicKeys;
   projectsList = [];
   filteredProjectsList = [];
   dataLoading = true;
-  subtopicsFilter:string[] = null;
 
-  constructor(private route: ActivatedRoute, private localJson: LocalJsonService, private searchService: SearchService, private router: Router, private location: Location, private aroute: ActivatedRoute, private urlService: UrlService) { }
+  subtopicsFilter: string[] = null;
 
-  filterProjectsList(event:any = null) {
+  constructor(
+    private route: ActivatedRoute,
+    private localJson: LocalJsonService,
+    private searchService: SearchService,
+    private router: Router,
+    private location: Location,
+    private aroute: ActivatedRoute,
+    private urlService: UrlService
+  ) {}
+
+  filterProjectsList(event: any = null) {
+    
     this.filteredProjectsList = [];
     for (var project in this.projectsList) {
-      if (this.current_subtopic != 'All Subtopics' && this.projectsList[project].subtopics != null) {
+      if (
+        this.current_subtopic != "All Subtopics" &&
+        this.projectsList[project].subtopics != null
+      ) {
         var matched_subtopic = false;
         for (var subtopic in this.projectsList[project].subtopics) {
-          if (this.projectsList[project].subtopics[subtopic] == this.current_subtopic) {
+          if (
+            this.projectsList[project].subtopics[subtopic] ==
+            this.current_subtopic
+          ) {
             matched_subtopic = true;
             break;
           }
@@ -104,52 +120,54 @@ export class TopicsComponent implements OnInit {
         }
       }
       var matchedType = false;
-      if (this.current_type != 'All Types') {
+      if (this.current_type != "All Types") {
         for (var thisType in this.projectsList[project].types) {
-          var matchedType = true
-          if (this.projectsList[project].types[thisType] !== this.current_type) {
-            matchedType = false
+          var matchedType = true;
+          if (
+            this.projectsList[project].types[thisType] !== this.current_type
+          ) {
+            matchedType = false;
           } else {
-            break
+            break;
           }
         }
         if (!matchedType) {
           continue;
         }
       }
-      if (this.current_fy != 'All Fiscal Years') {
+      if (this.current_fy != "All Fiscal Years") {
         if (this.projectsList[project].fiscal_year !== this.current_fy) {
           continue;
         }
       }
-      if (this.current_status != 'All Statuses') {
+      if (this.current_status != "All Statuses") {
         if (this.projectsList[project].status !== this.current_status) {
           continue;
         }
       }
-      if (this.current_csc != 'All CASCs') {
-        if (this.projectsList[project].csc['name'] !== this.current_csc) {
+      if (this.current_csc != "All CASCs") {
+        if (this.projectsList[project].csc["name"] !== this.current_csc) {
           continue;
         }
       }
       this.filteredProjectsList.push(this.projectsList[project]);
     }
-    this.updateUrl()
-    this.sortList()
+    this.updateUrl();
+    this.sortList();
   }
 
   showAllProjects() {
     this.filteredProjectsList = this.projectsList;
-    this.current_subtopic = 'All Subtopics';
-    this.current_fy = 'All Fiscal Years';
-    this.current_status = 'All Statuses';
-    this.current_type = 'Project';
-    this.current_csc = 'All CASCs';
+    this.current_subtopic = "All Subtopics";
+    this.current_fy = "All Fiscal Years";
+    this.current_status = "All Statuses";
+    this.current_type = "Project";
+    this.current_csc = "All CASCs";
   }
 
   isOnTopic(subtopic) {
     for (var topicSubtopic in this.subtopicsFilter) {
-      if (subtopic == this.subtopicsFilter[topicSubtopic]['label']) {
+      if (subtopic == this.subtopicsFilter[topicSubtopic]["label"]) {
         return true;
       }
     }
@@ -158,27 +176,26 @@ export class TopicsComponent implements OnInit {
 
   //TODO: put this code in a utility function/service
   updateUrl() {
-    let params: any = { };
-    if (this.current_subtopic != 'All Subtopics') {
-      params['subtopic'] = this.current_subtopic
+    let params: any = {};
+    if (this.current_subtopic != "All Subtopics") {
+      params["subtopic"] = this.current_subtopic;
     }
-    if (this.current_fy != 'All Fiscal Years') {
-      params['year'] = this.current_fy
+    if (this.current_fy != "All Fiscal Years") {
+      params["year"] = this.current_fy;
     }
-    if (this.current_status != 'All Statuses') {
-      params['status'] = this.current_status
+    if (this.current_status != "All Statuses") {
+      params["status"] = this.current_status;
     }
-    if (this.current_type != 'All Types') {
-      params['type'] = this.current_type
+    if (this.current_type != "All Types") {
+      params["type"] = this.current_type;
     }
-    if (this.current_csc != 'All CASCs') {
-      params['casc'] = this.current_csc
+    if (this.current_csc != "All CASCs") {
+      params["casc"] = this.current_csc;
     }
 
-    const url = this
-        .router
-        .createUrlTree([params], {relativeTo: this.aroute})
-        .toString();
+    const url = this.router
+      .createUrlTree([params], { relativeTo: this.aroute })
+      .toString();
 
     this.location.replaceState(url);
   }
@@ -186,27 +203,17 @@ export class TopicsComponent implements OnInit {
   sortList() {
     // First sorts projects by year, then by title
     this.filteredProjectsList.sort((a, b) => {
-      // function sortByCsc(a, b) {
-      //   let acsc = a.csc_name;
-      //   let bcsc = b.csc_name;
-
-      //   if(acsc == bcsc) {
-      //     return sortByTitle(a, b);
-      //   }
-      //   return (acsc < bcsc) ? -1 : (acsc > bcsc) ? 1 : 0;
-      // }
-
       function sortByTitle(a, b) {
         if (a.title == b.title) {
           return 0;
         }
-        return (a.title < b.title) ? -1 : 1;
+        return a.title < b.title ? -1 : 1;
       }
 
       if (a.fiscal_year == b.fiscal_year) {
-        return sortByTitle (a, b);
+        return sortByTitle(a, b);
       }
-      return (a.fiscal_year > b.fiscal_year) ? -1 : 1;
+      return a.fiscal_year > b.fiscal_year ? -1 : 1;
     });
   }
 
@@ -218,65 +225,78 @@ export class TopicsComponent implements OnInit {
     });
 
     this.sub = this.route.params.subscribe(params => {
-      this.topic = params['topic'];
-      if (params['subtopic']) {
-        this.current_subtopic = params['subtopic'];
+      this.topic = params["topic"];
+      if (params["subtopic"]) {
+        this.current_subtopic = params["subtopic"];
       }
-      if (params['year']) {
-        this.current_fy = params['year'];
+      if (params["year"]) {
+        this.current_fy = params["year"];
       }
-      if (params['status']) {
-        this.current_status = params['status'];
+      if (params["status"]) {
+        this.current_status = params["status"];
       }
-      if (params['type']) {
-        this.current_type = params['type'];
+      if (params["type"]) {
+        this.current_type = params["type"];
       }
-      if (params['csc']) {
-        this.current_csc = params['csc'];
+      if (params["csc"]) {
+        this.current_csc = params["csc"];
       }
+
       this.page_title = this.topic_names[this.topic]
       this.urlService.setPreviousTitle(this.page_title);
+
     });
     this.searchService.getTopics().subscribe(topics => {
       var topics = topics;
       for (var topic in topics) {
         if (topics[topic].label == this.page_title) {
-           this.subtopicsFilter = topics[topic].subtopics;
+          this.subtopicsFilter = topics[topic].subtopics;
         }
       }
     });
-    this.localJson.loadTopic(encodeURIComponent(this.topic_names[this.topic])).subscribe(data => {
-      this.projectsList = data;
-
+    this.localJson
+      .loadTopic(encodeURIComponent(this.topic_names[this.topic]))
+      .subscribe(data => {
+        this.projectsList = data;
         for (var project in this.projectsList) {
           for (var subtopic in this.projectsList[project].topics) {
             if (this.subtopicsFilter != null) {
               for (var topicSubtopic in this.subtopicsFilter) {
-                if ((this.projectsList[project].subtopics[subtopic] == this.subtopicsFilter[topicSubtopic]['label'])
-                  && (this.subtopics.indexOf(this.projectsList[project].subtopics[subtopic]) < 0)) {
-                  this.subtopics.push(this.projectsList[project].subtopics[subtopic])
+                if (
+                  this.projectsList[project].subtopics[subtopic] ==
+                    this.subtopicsFilter[topicSubtopic]["label"] &&
+                  this.subtopics.indexOf(
+                    this.projectsList[project].subtopics[subtopic]
+                  ) < 0
+                ) {
+                  this.subtopics.push(
+                    this.projectsList[project].subtopics[subtopic]
+                  );
                 }
               }
             }
           }
-          if (this.fiscal_years.indexOf(this.projectsList[project].fiscal_year) < 0) {
+          if (
+            this.fiscal_years.indexOf(this.projectsList[project].fiscal_year) <
+            0
+          ) {
             if (this.projectsList[project].fiscal_year != null) {
-              this.fiscal_years.push(this.projectsList[project].fiscal_year)
+              this.fiscal_years.push(this.projectsList[project].fiscal_year);
             }
           }
           if (this.statuses.indexOf(this.projectsList[project].status) < 0) {
             if (this.projectsList[project].status != null) {
-              this.statuses.push(this.projectsList[project].status)
+              this.statuses.push(this.projectsList[project].status);
             }
           }
-          if (this.cscs.indexOf(this.projectsList[project].csc['name']) < 0) {
-            this.cscs.push(this.projectsList[project].csc['name'])
+          if (this.cscs.indexOf(this.projectsList[project].csc["name"]) < 0) {
+            this.cscs.push(this.projectsList[project].csc["name"]);
           }
           if (this.projectsList[project].types) {
             for (var this_type of this.projectsList[project].types) {
               if (this.types.indexOf(this_type) < 0) {
                 if (this_type != null) {
-                  this.types.push(this_type)
+                  this.types.push(this_type);
                 }
               }
             }
@@ -288,9 +308,9 @@ export class TopicsComponent implements OnInit {
           this.cscs.sort();
 
           // After sorting, move All CASCs to top of sorted list.
-          var temp = this.cscs[0]
-          this.cscs[0] = this.cscs[1]
-          this.cscs[1] = temp
+          var temp = this.cscs[0];
+          this.cscs[0] = this.cscs[1];
+          this.cscs[1] = temp;
 
           this.filteredProjectsList.push(this.projectsList[project]);
           this.dataLoading = false;
@@ -299,48 +319,33 @@ export class TopicsComponent implements OnInit {
 
           // cscs and year
           for (var project in this.filteredProjectsList) {
-            this.projectsList[project].csc_name = this.projectsList[project].csc['name'];
-
-            // if (!this.projectsList[project].fiscal_year) {
-            //   this.projectsList[ project].fiscal_year="N/A";
-            // }
+            this.projectsList[project].csc_name = this.projectsList[
+              project
+            ].csc["name"];
 
             // subtopics
-            this.projectsList[project].subtopics_formatted = '';
+            this.projectsList[project].subtopics_formatted = "<ul>";
             for (var st of this.projectsList[project].subtopics) {
-              if ((this.isOnTopic(st)) && this.projectsList[project].subtopics_formatted.indexOf(st) < 0) {
-                this.projectsList[project].subtopics_formatted = this.projectsList[project].subtopics_formatted + st + '<br>';
+              if (
+                this.isOnTopic(st) &&
+                this.projectsList[project].subtopics_formatted.indexOf(st) < 0
+              ) {
+                this.projectsList[project].subtopics_formatted +=
+                  "<li>" + st + "</li>";
               }
             }
+            this.projectsList[project].subtopics_formatted += "</ul>";
 
             // status
             if (!this.projectsList[project].status) {
               this.projectsList[project].status = "N/A";
             }
-            // type
-            //if (!this.projectsList[project].types) {
-            //  this.projectsList[project].types = "N/A";
-            //}
-
-            // contains
-            this.projectsList[project].contains = '<div align="center">';
-
-            if (this.projectsList[project].hasFolders) {
-              this.projectsList[project].contains += '<i class="fa fa-folder fa-lg" title="This project has products." aria-hidden="true"></i>';
-            }
-            if (this.projectsList[project].hasMaps) {
-              this.projectsList[project].contains += '&nbsp&nbsp<i class="fa fa-map fa-lg" title="This project has maps." aria-hidden="true"></i>';
-            }
-
-            this.projectsList[project].contains += '</div>';
-          }//end for project
-
+          } //end for project
         }
 
-      this.current_type = 'Project'
-      this.filterProjectsList()
-      this.sortList()
-
-    });
+        this.current_type = "Project";
+        this.filterProjectsList();
+        this.sortList();
+      });
   }
 }
